8566f1e7acae5338dae3ae07d782f63060a3a25d
Mads Obitsø <madsobitsoe@gmail.com>
2021-01-29 18:27:51 +0100

    Add readline interface to repl

--- src/main.c
+++ src/main.c
@@ -5,62 +5,79 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <inttypes.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include "cpu.h"
 #include "nes.h"
-/* #include "rom.h" */
 #include "logger.h"
+
+
+void run_for_n_cycles(nes_state *state, uint32_t cycles) {
+  for (uint32_t i = 0; i < cycles; i++)
+    {
+      step(state);
+    }
+}
+
+int parse_cmd(char *line) {
+  // Quit on 0
+  if (strncmp(line, "q", 1) == 0) { return 0; }
+  // Step N on return N
+  if (strncmp(line, "s", 1) == 0) {
+    int steps = 1; // Default step
+    if (strlen(line) == 1) { return steps; }
+    // Check if there is a base 10 number supplied
+    steps = strtol(line+1, NULL, 10);
+    // return if conversion succeeded
+    if (steps > 0) { return steps; }
+    // If not, check if the input was in hex
+    steps = strtol(line+1, NULL, 16);
+    if (steps > 0) { return steps; }
+  }
+  // Invalid input on -1
+  return -1;
+}
 
 // gdb-like step interface
 void ndb(nes_state *state) {
-  // Print current state, i.e. registers
-  print_state(state);
-
-  /* uint8_t ni; */
-  /* uint16_t translated = translate_memory_location(state->current_opcode_PC); */
-  /* printf("Trans: 0x%04x\n", translated); */
-  // Print the next 20 bytes (instructions) after PC
-  /* if (translated <= 0x7ff) { */
-  /*   printf("reading from RAM\n"); */
-  /*   int counter = 0; */
-  /*   while (counter < 20) { */
-  /*     int incr_by = decode_instruction(state->memory[translated+counter - 0x8000], translated+counter); */
-  /*     counter += incr_by; */
-  /*   } */
-  /* } */
-  /* else */
-  /*   { */
-  /*     printf("reading from ROM\n"); */
-  /*     int counter = 0; */
-  /*     uint16_t offset = translated - 0xc000; */
-  /*     printf("Trying to read from 0x%04x.\n", offset); */
-  /*     while (counter < 20) { */
-  /*       int incr_by = decode_instruction(state->rom[offset+counter], translated+counter); */
-  /*       counter += incr_by; */
-  /*     } */
-  /*   } */
-  /* printf("\n"); */
-
-  print_log(state);
 
   // Get input from user
-  printf("s: step, q: quit\n");
-  char input;
-  scanf(" %c", &input);
-  switch(input) {
-  case 'q':
-    printf("Exiting..\n");
-    state->running = false;
-    break;
-  case 's':
-    printf("stepping.\n");
-    step(state);
-    break;
-  default:
-    printf("Invalid input\n");
+  char* linebuf;
+  int cmd;
+  while (state->running) {
+    // Print current state, i.e. registers
+    print_state(state);
+
+    print_log(state);
+
+    linebuf = readline("s [N]: step, q: quit >");
+    if (strlen(linebuf) > 0) {
+      add_history(linebuf);
+      cmd = parse_cmd(linebuf);
+    }
+    switch(cmd) {
+    case 0:
+      printf("Exiting..\n");
+      state->running = false;
+      break;
+    case (-1):
+      printf("Invalid input\n");
+      break;
+    case 1:
+      /* printf("stepping.\n"); */
+      step(state);
+      break;
+    default:
+      run_for_n_cycles(state, cmd);
+      break;
+    }
+    // readline malloc's a new linebuffer every time. Free it.
+    free(linebuf);
+
   }
-  if (state->running) { ndb(state); }
-  else { return; }
+  return;
 }
 
 
@@ -81,18 +98,11 @@ int load_rom(char *filename, unsigned char **rombuf) {
 
   fread(*rombuf, 1, romfilestat.st_size, infile);
   fclose(infile);
-  //  hexdump(*rombuf, romfilestat.st_size);
   return 0;
 
 }
 
 
-void run_for_n_cycles(nes_state *state, uint32_t cycles) {
-  for (uint32_t i = 0; i < cycles; i++)
-    {
-      step(state);
-    }
-}
 
 int main (int argc, char **argv) {
   int opt;
@@ -132,12 +142,6 @@ int main (int argc, char **argv) {
   // Now optind (declared extern int by <unistd.h>) is the index of the first non-option argument.
   // If it is >= argc, there were no non-option arguments.
 
-  /* printf ("interactive = %d, logfile = %s, cycles = %u\n", */
-  /*         interactive, logfile, cycles_to_run); */
-
-  /* for (int index = optind; index < argc; index++) */
-  /*   printf ("Non-option argument %s\n", argv[index]); */
-
   if (optind >= argc) {
     fprintf(stderr, "Usage: %s (<filename>|-)\n", argv[0]);
     return EXIT_FAILURE;
@@ -166,10 +170,6 @@ int main (int argc, char **argv) {
   reset(state);
   printf("Done!\n");
 
-  /* printf("Trying to read the initial value for PC with read_mem..\n"); */
-  /* uint16_t reset_vector_value = read_mem_short(state, 0xfffc); */
-  /* printf("Value: 0x%x\n", reset_vector_value); */
-  /* set_pc(state, reset_vector_value); */
   // The nestest.nes file requires PC to be set to this address after powerup.
   set_pc(state, 0xc000);
   if (interactive) {
