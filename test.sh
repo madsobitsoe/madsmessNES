b8cc61bcf0384a52eef67ca2c21b4575085b7b57
Mads Obitsø <madsobitsoe@gmail.com>
2021-02-02 17:44:15 +0100

    Start test at correct PC

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 CYCLES=9700
 LINES=$(wc testlog.log | cut -d' ' -f5)
-./emu -c $CYCLES test/nestest.nes
+./emu -s 0xc000 -c $CYCLES test/nestest.nes
 diff -c <(head -$LINES testlog.log) <(head -$LINES test/nestest.log)
